e3acdf46650a8bdce29d4b6509c3f5a11e09ba40
German Pineda <germanp173@gmail.com>
2019-01-27 19:45:46 -0800

    Decrease font size of score board

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -7,13 +7,13 @@ function ScoreBoard(props) {
             <p>Click each picture one time!</p>
             <div className="row">
                 <div className="col-sm-4">
-                    <h3>Score: <span>{props.score}</span></h3>
+                    <h4>Score: <span>{props.score}</span></h4>
                 </div>
                 <div className="col-sm-4">
-                    <h1>{props.result}</h1>        
+                    <h4>{props.result}</h4>
                 </div>
                 <div className="col-sm-4">
-                    <h3>Top Score: <span>{props.topScore}</span></h3>  
+                    <h4>Top Score: <span>{props.topScore}</span></h4>
                 </div>
             </div>
         </div>
